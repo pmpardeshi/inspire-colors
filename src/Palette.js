85ac300fa6097a6a71dca7e02d42dc5d0e7be16c
pmpardeshi <pramodmpardeshi@gmail.com>
2022-01-02 16:17:33 +0530

    added Palette Component

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Palette() {
+  return (
+    <div className="Palette">
+      {/* {navbar goes here} */}
+      <div className="Palette-colors">{/* {color boxes} */}</div>
+      {/* {footer} */}
+    </div>
+  );
+}
+
+export default Palette;
---
+++ src/seedColors.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    paletteName: "Five BLues",
+    id: "five-blues",
+    colors: [
+      { name: "navy", color: "#000080" },
+      { name: "steelblue", color: "#4682b4" },
+      { name: "dodgerblue", color: "#1e90ff" },
+      { name: "lightskyblue", color: "#87cefa" },
+      { name: "lightsteelblue", color: "#b0c4de" },
+    ],
+  },
+];
